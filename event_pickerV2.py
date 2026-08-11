02295f9ee8f0e1fc10877b2ad4c04ed76bd17d11
Limo-py <novak7franta@gmail.com>
2026-08-11 07:46:47 +0200

    Update event_pickerV2.py

--- event_pickerV2.py
+++ event_pickerV2.py
@@ -5,5 +5,11 @@ settlements = ["Bloomsville", "Kinkade port", "Spruceville", "Farmers rock"]
 
 # events and their stuff
 Bloomsville = {
-  
+  "nothing happened!" : {
+    "desc" : "today is a nice day isn't it?",
+    "cost" : {"money" : [0, 0], "iron" : [0, 0], "wood" : [0, 0]},
+    "weight" : 60
 }
+
+# randomly picking settlement
+settlement = random.choice(settlements)
