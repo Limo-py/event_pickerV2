1b3e8b6df6ff3f1eb12d0307084babcdff0ff31a
Limo-py <novak7franta@gmail.com>
2026-08-31 16:23:25 +0200

    Update event_pickerV2.py

--- event_pickerV2.py
+++ event_pickerV2.py
@@ -9,9 +9,29 @@ sys.stdout = catched_output = io.StringIO()
 # events and their stuff
 bloomsville = {
   "nothing happened!" : {
-    "desc" : "today is a nice day isn't it?",
-    "cost" : {"money" : [0, 0], "iron" : [0, 0], "copper" : [0, 0], "steel" : [0, 0], "wood" : [0, 0]},
-    "weight" : 60
+    "desc" : "Today is a nice day isn't it?",
+    "cost" : {"money" : [0, 0], "iron" : [0, 0], "steel" : [0, 0], "wood" : [0, 0]},
+    "weight" : 20},
+  "bridge collapsed" : {
+    "desc" : "The bridge has collapsed! We lost one snail who was on it. Rest in peace!",
+    "cost" : {"money" : [50, 250], "iron" : [0, 3], "steel" : [0, 2], "wood" : [5, 30]},
+    "weight" : 20},
+  "mayor drunk" : a {
+    "desc" : "Mayor was drunk while operating the ship! Don't worry, he's still alive",
+    "cost" : {"money" : [100, 100], "iron" : [0, 1], "steel" : [0, 0], "wood" : [0, 5]},
+    "weight" : 30},
+  "trading station on fire" : {
+    "desc" : "The trading station is on fire! Locals think it was JKI, but no evidence was found",
+    "cost" : {"money" : [100, 500], "iron" : [5, 15], "steel" : [1, 5], "wood" : [20, 30]},
+    "weight" : 10},
+  "tornado visited town" : {
+    "desc" : "Tornado went straight through the town and destroyed half of the it!",
+    "cost" : {"money" : [500, 2000], "iron" : [10, 30], "steel" : [5, 10], "wood" : [30, 60]},
+    "weight" : 5},
+  "pirate attack" : {
+    "desc" : "Pirates attacked! We managed to defend ourselves, but they still destroyed part of the town!"
+    "cost" : {"money" : [75, 350], "iron" : [0, 10], "steel" : [0, 1], "wood" : [0, 20]},
+    "weight" : 15}
 }
 
 # randomly picking settlement
