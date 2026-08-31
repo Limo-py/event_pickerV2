44b9803442e965d8b1f0c211dbd8dbb6f44fb32b
Limo-py <novak7franta@gmail.com>
2026-08-31 15:38:46 +0200

    Update event_pickerV2.py

--- event_pickerV2.py
+++ event_pickerV2.py
@@ -1,4 +1,10 @@
 import random
+import requests
+import sys
+import io
+WEBHOOK_URL = "https://discord.com/api/webhooks/1536032910514196630/2JEuaUME5W6DtbBXxRfKGbjnTg7soBQjrGmwtHeTG39uJ8ww7tekr9m4DeO2IU-BehZp"
+old_stdout = sys.stdout
+sys.stdout = catched_output = io.StringIO()
 
 #list of settlements
 settlements = ["Bloomsville", "Kinkade port", "Spruceville", "Farmers rock"]
